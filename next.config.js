7e1387e0180a554b989bac308d9eff0ecd42e947
thinhnguyen1105 <thinhnguyen.teamx@gmail.com>
2023-04-07 00:08:57 +0700

    ignore lint build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   poweredByHeader: false,
   trailingSlash: true,
   basePath: '',
