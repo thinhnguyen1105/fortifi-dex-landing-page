57d237fb3d82a7ee5b0a75940681881199226a52
thinhnguyen1105 <thinhnguyen.teamx@gmail.com>
2023-04-09 13:59:41 +0700

    add ant design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 
 import '../styles/global.css';
+import 'antd/dist/reset.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
